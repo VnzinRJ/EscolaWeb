a9f84832dda5301dbe9c4fb4a9ada4846edce20e
teste <169127512022.5@senacrjedu.local>
2023-03-27 21:41:48 -0300

    escolaweb

---
+++ 2022.5_escolaweb_java-fabiano-aula08/src/br/com/escolaweb/services/AlunoService.java
@@ -0,0 +1,11 @@
+package br.com.escolaweb.services;
+
+import br.com.escolaweb.model.Aluno;
+
+public class AlunoService extends DAO {
+   
+    public void add(Aluno aluno){
+        Conectar();
+    }
+    
+}
